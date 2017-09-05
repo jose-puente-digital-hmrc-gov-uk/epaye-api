0f9db6822fed7d57685335ef89cc7ced2a41a4c6
Alen Mujezinovic <flashingpumpkin@gmail.com>
2017-09-05 10:15:38 +0100

    [YTA-2932] Revert test to original state

--- test/uk/gov/hmrc/epayeapi/controllers/GetTotalsSpec.scala
+++ test/uk/gov/hmrc/epayeapi/controllers/GetTotalsSpec.scala
@@ -16,17 +16,13 @@
 
 package uk.gov.hmrc.epayeapi.controllers
 
-import akka.stream.Materializer
-import akka.util.ByteString
 import org.mockito.Matchers._
 import org.mockito.Mockito.{reset, when}
-import org.scalatest.{BeforeAndAfterEach, fixture}
+import org.scalatest.BeforeAndAfterEach
 import play.api.Application
 import play.api.inject.bind
-import play.api.inject.guice.GuiceApplicationBuilder
-import play.api.libs.streams.Accumulator
 import play.api.mvc.Result
-import play.api.test.{FakeRequest, Helpers}
+import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import uk.gov.hmrc.auth.core.{ConfidenceLevel, Enrolment, EnrolmentIdentifier}
 import uk.gov.hmrc.domain.EmpRef
@@ -39,9 +35,35 @@ import scala.concurrent.Future
 import scala.concurrent.Future.successful
 
 class GetTotalsSpec extends AppSpec with BeforeAndAfterEach {
+  val ton = EnrolmentIdentifier("TaxOfficeNumber", "840")
+  val tor = EnrolmentIdentifier("TaxOfficeReference", "GZ00064")
+  val empRef = EmpRef(ton.value, tor.value)
+
+  implicit val hc = HeaderCarrier()
+  val http = mock[WSHttp]
+
+  val app = builder
+    .update(_.overrides(bind(classOf[WSHttp]).toInstance(http)))
+
+  val activeEnrolment =
+    AuthOk(Enrolment("IR-PAYE", Seq(ton, tor), "activated", ConfidenceLevel.L300))
+
+  val inactiveEnrolment =
+    AuthOk(activeEnrolment.data.copy(state = "inactive"))
+
+  val differentEnrolment =
+    AuthOk(Enrolment("IR-Else", Seq(ton, tor), "activated", ConfidenceLevel.L300))
+
+  def request(implicit a: Application): Future[Result] =
+    inject[GetTotals].getTotals(empRef)(FakeRequest())
+
+
+  override protected def beforeEach(): FixtureParam = {
+    reset(http)
+  }
 
   "The Totals endpoint" should {
-    "return 200 OK on active enrolments" in new Setup {
+    "return 200 OK on active enrolments" in new App(app.withAuth(activeEnrolment).build) {
       when(http.GET[HttpResponse](anyString)(anyObject(), anyObject())).thenReturn {
         successful {
           HttpResponse(200, responseString = Some(""" {"credit": 100, "debit": 0} """))
@@ -49,40 +71,13 @@ class GetTotalsSpec extends AppSpec with BeforeAndAfterEach {
       }
       status(request) shouldBe OK
     }
-    "return 403 Forbidden on inactive enrolments" in new Setup {
+    "return 403 Forbidden on inactive enrolments" in new App(app.withAuth(inactiveEnrolment).build) {
       status(request) shouldBe FORBIDDEN
     }
-    "return 403 Forbidden with different enrolments" in new Setup {
+    "return 403 Forbidden with different enrolments" in new App(app.withAuth(differentEnrolment).build) {
       status(request) shouldBe FORBIDDEN
     }
   }
 
-  trait Setup extends fixture.NoArg {
-    val ton = EnrolmentIdentifier("TaxOfficeNumber", "840")
-    val tor = EnrolmentIdentifier("TaxOfficeReference", "GZ00064")
-    val empRef = EmpRef(ton.value, tor.value)
-    val http: WSHttp = mock[WSHttp]
 
-    implicit val hc: HeaderCarrier = HeaderCarrier()
-    implicit lazy val app: Application = new GuiceApplicationBuilder().overrides(bind(classOf[WSHttp]).toInstance(http)).build()
-
-    val activeEnrolment =
-      AuthOk(Enrolment("IR-PAYE", Seq(ton, tor), "activated", ConfidenceLevel.L300))
-
-    val inactiveEnrolment =
-      AuthOk(activeEnrolment.data.copy(state = "inactive"))
-
-    val differentEnrolment =
-      AuthOk(Enrolment("IR-Else", Seq(ton, tor), "activated", ConfidenceLevel.L300))
-
-    def request(implicit a: Application): Future[Result] =
-      inject[GetTotals].getTotals(empRef)(FakeRequest()).run()(
-        inject[Materializer]
-      )
-
-    override def apply(): Unit = {
-      def callSuper = super.apply()
-      Helpers.running(app)(callSuper)
-    }
-  }
 }
